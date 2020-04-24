d78737b9cccc658fb36693081c39318f8a32e327
Patrick Wake <Patrick.Wake@fdmgroup.com>
2020-04-24 13:22:25 +0100

    Ready to hand in!

--- WpfRecon/Models/LocalAddress.cs
+++ WpfRecon/Models/LocalAddress.cs
@@ -6,11 +6,14 @@ namespace WpfRecon.Models
 {
     class LocalAddress
     {
+        //Get local Ipv4 adress from local machine
         public static string GetLocalIPAddress()
         {
+
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)
             {
+                //Selects IPV4 
                 if (ip.AddressFamily == AddressFamily.InterNetwork)
                 {
                     return ip.ToString();
